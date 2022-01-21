7f77b40c88b048bb1b792c20e3f412abdb625e58
dmshrtnto <dimas.hariyanto@ilcs.co.id>
2022-01-21 18:08:33 +0700

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  
+  constructor(props){
+    super(props)
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+  
+  changeHandler = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  submitHandler = e =>{
+    e.preventDefault()
+    console.log(this.state)
+    axios.post('http://localhost:9000/api/op_list_user', this.state)
+    .then(response =>{
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  render(){
+    const {username,password} = this.state
+    return (
+      <div className="App">
+        <form onSubmit={this.submitHandler}>
+          <div>
+            <input type="text" name = "username" value={username} onChange={this.changeHandler}/>
+          </div>
+          <div>
+            <input type="text" name = "password" value={password} onChange={this.changeHandler}/>
+          </div>
+          <button type = "submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
-
 export default App;
